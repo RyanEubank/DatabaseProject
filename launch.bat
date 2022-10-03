0f8044f495e9e925760b1cc21d5a156621a188ac
REubank <reubank79@gmail.com>
2022-10-03 08:36:15 -0500

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,4 +1,4 @@
-set PATH_TO_JAVAFX="C:\Users\Ryan\Desktop\Test\lib\javafx-sdk-19\lib"
+set PATH_TO_JAVAFX="%cd%\lib\javafx-sdk-19\lib"
 javac --module-path %PATH_TO_JAVAFX% --add-modules javafx.controls src/Main.java
 java --module-path %PATH_TO_JAVAFX% --add-modules javafx.controls -classpath src Main
 pause
