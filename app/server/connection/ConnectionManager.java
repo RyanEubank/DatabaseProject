6e6aeabc08d82b1ea7349e30bc165c7a7adde500
REubank <reubank79@gmail.com>
2022-10-04 16:37:15 -0500

    Add dependency JDBC
    Setup classpath to load required jdbc drivers for mysql and added initial connection manager

---
+++ app/server/connection/ConnectionManager.java
@@ -0,0 +1,86 @@
+package app.server.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionManager {
+	
+	// Singleton instance for the connection manager
+	private static ConnectionManager INSTANCE;
+	
+	private String m_database; 			// name of the database host
+	private String m_port;				// port number for the database
+	private Connection m_connection;	// connection interface to the database
+	private Properties m_properties;	// properties for the database connection
+	
+	/**
+	 * Constructs a new ConnectionManager by reading the system configuration
+	 * files to get the database host name and port number then establishing
+	 * a new connection via JDBC.
+	 */
+	private ConnectionManager() {
+		// this step is not strictly required as of JDBC 4, but included just in case
+		registerJDBCDriver();
+		
+		//TODO read in the database and port number instead of hard-coding values.
+		this.m_database = "localhost";
+		this.m_port = "3306";
+		
+		makeConnection();
+	}
+
+	/**
+	 * Loads and registers the mysql jdbc driver required to connect
+	 * to a mysql database. Logs an error and exits if the driver cannot
+	 * be loaded.
+	 */
+	private void registerJDBCDriver() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch(ClassNotFoundException e) {
+			System.out.println("Error: Unable to register ConnectorJ Drivers.");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	
+	/**
+	 * Attempts to connect to the library database. Logs an error and exits
+	 * if the connection cannot be made.
+	 */
+	private void makeConnection() {
+		String url = "jdbc:mysql://" + this.m_database + ":" + this.m_port + "/";
+		try {
+			this.m_connection = DriverManager.getConnection(url, m_properties);
+		} catch (SQLException e) {
+			System.out.println("Error: Unable to establish connection with database.");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Get method to retrieve the database connection for the system.
+	 * 
+	 * @return
+	 * 	The connection instance to the library database.
+	 */
+	public Connection getConnection() {
+		return this.m_connection;
+	}
+	
+	/**
+	 * Returns an instance of the connection manager, or creates a new
+	 * instance if it does not yet exist: i.e. the first call to this
+	 * method.
+	 * 
+	 * @return
+	 * 	a static instance of the connection manager for the library system.
+	 */
+	public static ConnectionManager getSingleton() {
+		if (INSTANCE == null)
+			INSTANCE = new ConnectionManager();
+		return INSTANCE;
+	}
+}
--- launch.bat
+++ launch.bat
@@ -1,4 +1,6 @@
-set PATH_TO_JAVAFX="%cd%\lib\javafx-sdk-19\lib"
-javac --module-path %PATH_TO_JAVAFX% --add-modules javafx.controls src/Main.java
-java --module-path %PATH_TO_JAVAFX% --add-modules javafx.controls -classpath src Main
+set PATH_TO_FX="lib/javafx-sdk-19/lib"
+set PATH_TO_DRIVER="C:\Program Files (x86)\MySQL\Connector J 8.0\mysql-connector-java-8.0.30.jar"
+
+javac --module-path %PATH_TO_FX% --add-modules javafx.controls -classpath %cd%;%PATH_TO_DRIVER% app/MainApplication.java
+java --module-path %PATH_TO_FX% --add-modules javafx.controls -classpath %cd%;%PATH_TO_DRIVER% app.MainApplication
 pause
