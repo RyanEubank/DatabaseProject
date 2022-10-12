c3fae48c6848e143321f0c6a8da6acf5f7bbec94
REubank <reubank79@gmail.com>
2022-10-12 13:23:03 -0500

    Add connectorJ to lib folder

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 set PATH_TO_FX="lib/javafx-sdk-19/lib"
-set PATH_TO_DRIVER="C:\Program Files (x86)\MySQL\Connector J 8.0\mysql-connector-java-8.0.30.jar"
+set PATH_TO_DRIVER="lib/connector-j-8.0/mysql-connector-j-8.0.31.jar"
 
 javac --module-path %PATH_TO_FX% --add-modules javafx.controls -classpath %cd%;%PATH_TO_DRIVER% app/MainApplication.java
 java --module-path %PATH_TO_FX% --add-modules javafx.controls -classpath %cd%;%PATH_TO_DRIVER% app.MainApplication
