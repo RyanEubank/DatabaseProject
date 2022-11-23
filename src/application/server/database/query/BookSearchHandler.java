4e62ca7eacdb1561deaeaf4547030e9597df6900
REubank <reubank79@gmail.com>
2022-11-23 17:53:58 -0600

    Made things pretty again

--- src/application/server/database/query/BookSearchHandler.java
+++ src/application/server/database/query/BookSearchHandler.java
@@ -7,10 +7,10 @@ public class BookSearchHandler extends AbstractSearchHandler<BookSearchResult> {
 
 	@Override
 	protected void setQuery(String filter) {
-		this.m_query =  "SELECT b.isbn, b.title, a.name, IF(bl.isbn IS NULL OR bl.date_in IS NOT NULL, TRUE, FALSE) as isAvailable "
+		this.m_query =  "SELECT b.isbn, b.title, a.name, IF(bl.isbn IS NULL, TRUE, FALSE) as isAvailable "
 				+ "FROM ((Library.Book AS b JOIN Library.Book_Authors AS ba ON b.isbn = ba.isbn) "
 				+ "JOIN Library.Authors as a ON ba.author_id = a.author_id) "
-				+ "LEFT JOIN Library.Book_loans AS bl ON b.isbn = bl.isbn";
+				+ "LEFT JOIN (SELECT * FROM Library.Book_Loans as temp WHERE temp.date_in IS NULL) AS bl ON b.isbn = bl.isbn ";
 	}
 
 	@Override
