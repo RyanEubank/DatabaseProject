9536f5094a0c4a2e77cdf6d30ce64cec52ac5197
REubank <reubank79@gmail.com>
2022-11-24 16:08:04 -0600

    Started borrower management

--- src/application/client/scenes/controllers/BorrowerManagementPane.java
+++ src/application/client/scenes/controllers/BorrowerManagementPane.java
@@ -2,9 +2,14 @@ package src.application.client.scenes.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import src.application.server.database.query.LibraryCardManager;
 
 public class BorrowerManagementPane extends AbstractPane {
 
+	@FXML
+	private AnchorPane borrower_pane;
 	@FXML
 	private TextField user_ssn_field;
 	@FXML
@@ -16,21 +21,85 @@ public class BorrowerManagementPane extends AbstractPane {
 	@FXML
 	private TextField user_phone_field;
 	
+	
 	@Override
 	public void postInitialize() {
 		super.postInitialize();
 	}
 
+	/**
+	 * Creates a new user in the library database.
+	 */
 	@Override
 	protected void onPerformAction() {
 		this.m_parent.setActionError("");
+		
 		String ssn = this.user_ssn_field.getText();
 		String fname = this.user_fname_field.getText();
-		String lName = this.user_lname_field.getText();
+		String lname = this.user_lname_field.getText();
 		String address = this.user_address_field.getText();
 		String phone = this.user_phone_field.getText();
+		
+		createUser(ssn, fname, lname, address, phone);
 	}
 	
+	/**
+	 * Gathers the information entered by the user and 
+	 * displays an error if there is missing information.
+	 * If all information is provided the method attempts
+	 * to create a new user in the library database.
+	 */
+	private void createUser(
+		String ssn,
+		String fname,
+		String lname,
+		String address,
+		String phone
+	) throws Exception {
+		if (isRequiredFieldMissing(ssn, fname, lname, address))
+			setErrorLabels();
+		else {
+			clearErrorLabels();
+			new LibraryCardManager().createUser(ssn, fname, lname, address, phone);
+		}
+	}
+
+	/**
+	 * Sets the error text if there are missing fields
+	 * of the required information for creating a new user.
+	 */
+	private void setErrorLabels() {
+		this.borrower_pane.getChildren().stream()
+			.filter((c) -> c instanceof Text)
+			.forEach((label) -> ((Text)label).setText("*Required Field"));
+	}
+
+	/**
+	 * Clears the text from the error labels next to the 
+	 * required fields.
+	 */
+	private void clearErrorLabels() {
+		this.borrower_pane.getChildren().stream()
+			.filter((c) -> c instanceof Text)
+			.forEach((label) -> ((Text)label).setText(""));
+	}
+	
+	/**
+	 * Returns whether any of the provided fields are
+	 * empty or blank.
+	 * 
+	 * @param fields - the list of required fields.
+	 * 
+	 * @return
+	 *  Returns true if all of te required fields are present.
+	 */
+	private boolean isRequiredFieldMissing(String... fields) {
+		for (String field : fields) {
+			if (field.isEmpty() || field.isBlank())
+				return true;
+		}
+		return false;
+	}
 
 	/**
 	 * Returns the name of the action performed by the borrower
--- src/application/server/database/query/LibraryCardManager.java
+++ src/application/server/database/query/LibraryCardManager.java
@@ -1,11 +1,28 @@
 package src.application.server.database.query;
 
-public class LibraryCardManager {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-	public static void createUser(
-		String ssn, String firstname, String lastname, String address, String phone
-	) {
+public class LibraryCardManager extends AbstractUpdateHandler {
+
+	public int createUser(
+		String ssn, String firstname, 
+		String lastname, String address, String phone
+	) throws Exception {
 		System.out.println("New user added: " + ssn + " " + firstname + " " + lastname + " " + address + " " + phone);
+		String fullName = firstname + " " + lastname;
+		return onExecuteWithException(ssn, fullName, address, phone);
+	}
+
+	@Override
+	protected String getQuery() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void setSubqueries(PreparedStatement statement, Object... subqueries) throws SQLException {
+		// TODO Auto-generated method stub
 		
 	}
 
